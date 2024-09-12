7815a7e27d48465ffef81d74870def1c3a07d561
RJH <rjh5957@naver.com>
2024-09-12 10:07:15 +0900

    Proto-type 1

--- esper_sourcecode/fletApp.py
+++ esper_sourcecode/fletApp.py
@@ -2,23 +2,17 @@ import flet as ft
 import time
 import smi2sublist as ss
 
+wid=750
+hig=400
+
 def main(page: ft.Page):
     page.title="E.S.P.ER."
     page.padding=0
-    page.window.width=600
-    page.window.height=270
-    page.bgcolor = ft.colors.GREY_900
-
-    page.window.frameless=True
-    page.window.center()
-
-    #page.vertical_alignment=ft.alignment.center
-    #page.horizontal_alignment=ft.alignment.center
-    #page.window.full_screen=True
-    #page.theme = ft.Theme(color_scheme_seed="black")
+    page.bgcolor = ft.colors.BLACK
 
     def begin_play(e):
         now.value=""
+        #now.value=page.width
         page.update()
         pre=0
         sub=ss.smi2sub(filename.value)
@@ -35,10 +29,10 @@ def main(page: ft.Page):
     title=ft.Text("E.S.P.ER.", size=20, weight=ft.FontWeight.BOLD)
     filename=ft.TextField(label="path")
     play=ft.ElevatedButton(text="PLAY", on_click=begin_play)
+
     now=ft.Text("NO SUBTITLES",
                 color=ft.colors.WHITE,
-                size = 20,
-                #theme_style=ft.TextThemeStyle.DISPLAY_SMALL,
+                size = 30,
                 text_align=ft.TextAlign.CENTER)
     
     set_compo = ft.Column(
@@ -46,34 +40,40 @@ def main(page: ft.Page):
         alignment=ft.alignment.center
 
     )
+
     settings=ft.Container(
+        width=wid/4,
+        height=hig,
         margin=ft.margin.all(0),
         padding=20,
-        width=200,
-        height=270,
         content=set_compo,
         alignment=ft.alignment.center,
         bgcolor=ft.colors.WHITE,
     )
 
     printer=ft.Container(
+        width=wid,
+        height=hig,
         margin=ft.margin.all(0),
         padding=50,
-        width=600,
-        height=270,
         content=now,
         alignment=ft.alignment.top_center,
         bgcolor=ft.colors.BLACK,
     )
 
-    page.add(
-        ft.Row(
-            width=600,
+    all=ft.Container(
+        width=wid,
+        height=hig,
+        padding=0,
+        content=ft.Row(
             spacing=0,
             scroll=ft.ScrollMode.ALWAYS,
             controls=[settings,printer,],
-        )
+        ),
+        bgcolor=ft.colors.BLACK,
     )
+
+    page.add(all)
     page.update()
-    
+
 ft.app(main)
